723abe051d1a94863ef6e57ee7271200cec4aff6
Demetrio-Admin <tequiero4522@gmail.com>
2026-09-10 16:43:27 +0500

    Fix Phaser 4 namespace import

--- infinite-road/src/main.ts
+++ infinite-road/src/main.ts
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import * as Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene';
 import { MainMenuScene } from './scenes/MainMenuScene';
 import { RoadScene } from './scenes/RoadScene';
