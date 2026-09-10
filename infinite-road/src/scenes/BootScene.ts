482e793c461a28789c01ccec9a287d83bf383c8b
Demetrio-Admin <tequiero4522@gmail.com>
2026-09-10 14:06:03 +0500

    Add boot scene

---
+++ infinite-road/src/scenes/BootScene.ts
@@ -0,0 +1,28 @@
+import Phaser from 'phaser';
+
+export class BootScene extends Phaser.Scene {
+  constructor() {
+    super('BootScene');
+  }
+
+  create() {
+    this.cameras.main.setBackgroundColor('#090b10');
+
+    const title = this.add.text(195, 360, 'БЕСКОНЕЧНЫЙ ПУТЬ', {
+      fontFamily: 'Georgia, serif',
+      fontSize: '28px',
+      color: '#f2ead7',
+      letterSpacing: 2,
+    }).setOrigin(0.5);
+
+    title.setAlpha(0);
+    this.tweens.add({
+      targets: title,
+      alpha: 1,
+      duration: 650,
+      yoyo: true,
+      hold: 350,
+      onComplete: () => this.scene.start('MainMenuScene'),
+    });
+  }
+}
