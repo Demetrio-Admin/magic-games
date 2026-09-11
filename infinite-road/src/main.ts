28e770ba6b17de449adb8f7d8a44fae9bae76d30
Demetrio-Admin <tequiero4522@gmail.com>
2026-09-11 17:04:31 +0500

    Restore Phaser 4 compatible render config

--- infinite-road/src/main.ts
+++ infinite-road/src/main.ts
@@ -5,14 +5,11 @@ import { RoadScene } from './scenes/RoadScene';
 import { BattleScene } from './scenes/BattleScene';
 import { RewardScene } from './scenes/RewardScene';
 
-const renderScale = Math.min(window.devicePixelRatio || 1, 2);
-
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.CANVAS,
   parent: 'app',
   width: 390,
   height: 844,
-  resolution: renderScale,
   backgroundColor: '#071516',
   scene: [RoadScene, BattleScene, RewardScene, MainMenuScene, BootScene],
   scale: {
